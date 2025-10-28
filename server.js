562ae560eb2785bc94b08382acaf6b321ecceb6f
ugeshpraavin <ugesh.240161@csbs.ritchennai.edu.in>
2025-10-28 12:38:52 +0530

    added 0.0.01

--- server.js
+++ server.js
@@ -98,6 +98,8 @@ app.get("/health", async (req, res) => {
 });
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
